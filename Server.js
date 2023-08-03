42cf3c950468418a33202f75f15ba3a6016faf33
Nilesh Kumar <137385597+nk9000@users.noreply.github.com>
2023-08-03 21:47:14 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -4,10 +4,17 @@ const cors = require("cors")
 const routes = require("./routes/ToDoRoute")
 require("dotenv").config()
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT | 5000
 app.use(express.json())
 app.use(cors())
-mongoose.connect(process.env.MONGODB_URL).then(()=>console.log("connected to mongodb")).catch((err)=>console.log("error in db"))
- 
+mongoose
+    .connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Mongodb Connected..."))
+    .catch((err) => console.error(err));
+
+
 app.use(routes)
 app.listen(PORT , ()=>console.log(`Listening on: ${PORT}`))
